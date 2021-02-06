7aae412f5b5669fefdbef7799bb8f9e46da41892
Laura Bilíkova <bilikovalaura@bilikova.onmicrosoft.com>
2021-02-06 18:44:59 +0100

    Finally

---
+++ FastClock/FastClock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FastClock
+{
+    class FastClock
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            while (z != 24)
+            {
+                x = ++x;
+                if (x == 60)
+                {
+                    x = 0;
+                    y = ++y;
+                    if (y == 24)
+                    {
+                        y = 0;
+                        z = ++z;
+                    }
+                }
+
+                Console.WriteLine(z + ":" + y + ":" + x);
+            }
+        }
+    }
+}
